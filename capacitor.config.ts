ab20b1260c1627e73482eab90d38c10e0d423a8f
David O Connor <G00400530@atu.ie>
2023-03-29 14:21:34 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'G00400530',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
